02f7eb3ce5043d211172f00d0a9ff306f1948803
Brett Pittman <agbrettpittman@gmail.com>
2022-12-19 10:02:45 -0600

    Fixed scrollbar

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import LandingPage from '../components/PseudoPages/LandingPage'
 import HomePage from '../components/PseudoPages/HomePage'
 import styled from 'styled-components'
 
-
-
 export default function Home() {
 
     return (
