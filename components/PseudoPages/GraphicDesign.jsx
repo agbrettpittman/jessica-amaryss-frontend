b1441a6f8d0735ef29af4a104716d786c55df784
Brett Pittman <agbrettpittman@gmail.com>
2022-12-22 17:44:39 -0600

    Built Content Pages

---
+++ components/PseudoPages/GraphicDesign.jsx
@@ -0,0 +1,16 @@
+
+import styled from 'styled-components'
+
+const MainPageBackground = styled.div`
+    z-index: 2;
+    background-color: red;
+    width: 100%;
+    min-height: 200vh;
+`
+
+export default function() {
+
+    return (
+        <h1>Graphic Design</h1>
+    )
+}
